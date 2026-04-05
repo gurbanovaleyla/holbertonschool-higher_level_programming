cbb9da1a690430af9c4934d0d9df847fa44ba2b7
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-04-05 22:04:10 +0200

    file added

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 = str1 + " " + str2
-print(f"Welcome to {str1}!")
+str1 = "Python is an interpreted language"
+str2 = "that allows you to quickly write a script or program"
+str3 = "one of the most popular languages"
+print(str3[12:] + " " + str2[4:40] + " " + str1[:6])
