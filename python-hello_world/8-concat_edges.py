600528751b5a3014c565129d5c313a14141c5a4a
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-04-05 22:06:12 +0200

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-print(word)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:67] + str[107:112] + str[:6])
