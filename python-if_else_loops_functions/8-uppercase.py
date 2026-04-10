0f6336a5f41fc6b5074057f2d774528e304e7a16
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-04-10 18:10:07 +0200

    Converting ,owercases to uppercases

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 def uppercase(str):
     result = ""
     for c in str:
-        if ord(c) >= 99 and ord(c) <= 122:
+        if ord(c) >= 97 and ord(c) <= 122:
             result += chr(ord(c) - 32)
         else:
             result += c
