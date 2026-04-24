edc6880e3cbd41a7e56d7585b10b86098562a264
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-04-24 06:53:53 +0200

    Finding square of each element in a matrix and returning a new matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in range(len(matrix)):
+        new_row = []
+        for element in range(len(matrix[row])):
+            new_row.append(matrix[row][element] ** 2)
+        new_matrix.append(new_row)
+    return new_matrix
