1cf62ddc6b37635586b3e1859af036591556c880
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-04-24 09:47:45 +0200

    Finding max value of a key and returning the key

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     best_key = None
     best_value = None
     for key in a_dictionary:
-        if a_dictionary[key] > best_value:
+        if best_value is None or a_dictionary[key] > best_value:
             best_value = a_dictionary[key]
             best_key = key
     return best_key
