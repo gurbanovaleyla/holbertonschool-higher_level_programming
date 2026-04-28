9befbe753a48c80859d1ff4ca03d3cda25719aae
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-04-28 20:08:43 +0200

    files added

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -22,4 +22,7 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("My name is {} {}".format(first_name, last_name))
+    result = "My name is {} {}".format(first_name, last_name)
+    if last_name != ""
+    else "My name is {}".format(first_name)
+    print(result)
