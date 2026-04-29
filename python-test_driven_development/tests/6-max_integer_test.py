c6bba0eedf9d6f118acb335cb5acfb6ab313fa42
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-04-29 16:12:06 +0200

    files added

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Unittest for max_integer function
+"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Test cases for max_integer"""
+
+    def test_ordered_list(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_unordered_list(self):
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -3, -5]), -1)
+
+    def test_single_element(self):
+        self.assertEqual(max_integer([7]), 7)
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_all_equal(self):
+        self.assertEqual(max_integer([5, 5, 5]), 5)
+
+    def test_float_values(self):
+        self.assertEqual(max_integer([1.5, 2.2, 0.9]), 2.2)
+
+
+if __name__ == '__main__':
+    unittest.main()
