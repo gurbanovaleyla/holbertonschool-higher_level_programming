20043baf52fdf706cbf326d24947c52a73c8c2e9
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-05-13 15:52:06 +0200

    defining a square by size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class by size which represents a square shape."""
+
+
+class Square:
+    """A class that represents a square."""
+
+    def __init__(self, size):
+        """Initialize a Square with a private size attribute."""
+        self.__size = size
