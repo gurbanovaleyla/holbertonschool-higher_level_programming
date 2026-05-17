34a1233a959a4a354557fc459845852c61616ce8
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-05-17 17:26:00 +0200

    Printing a list in a sorted way.

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains a class that inherits out of a list
+and prints the list in the ascending sort."""
+
+
+class MyList(list):
+    """A class inherits out of list."""
+    def print_sorted(self):
+        """Print list in the ascending order."""
+        print(sorted(self))       
