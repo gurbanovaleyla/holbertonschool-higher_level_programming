691627e1ed8a7409777e54a1af316104ea25efd9
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-05-21 14:49:48 +0200

    Return dictionary representation of Student instance

---
+++ python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Student class definition with attribute filtering.
+"""
+
+
+class Student:
+    """Defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return dictionary representation of Student instance.
+
+        If attrs is a list of strings, only those attributes are returned.
+        Otherwise, all attributes are returned.
+        """
+        obj_dict = self.__dict__
+
+        if isinstance(attrs, dict):
+            filtered_dict = {}
+            for key in attrs:
+                if key in obj_dict:
+                    filtered_dict[key] = obj_dict[key]
+            return filtered_dict
+
+        return obj_dict
