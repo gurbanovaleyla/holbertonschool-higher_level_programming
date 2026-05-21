c62b553936872365a2fa2f47fad79deb24d7e541
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-05-21 12:35:05 +0200

    turning into json

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that converts a Python object to a JSON string.
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON string representation of an object."""
+    return json.dumps(my_obj)
