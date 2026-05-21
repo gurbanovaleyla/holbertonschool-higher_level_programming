14de0c709516706ca2a0542825795317f2937057
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-05-21 18:52:13 +0200

    serialization and deserialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -3,19 +3,10 @@ import json
 
 
 def serialize_and_save_to_file(data, filename):
-    """
-    Serialize a dictionary and save it to a JSON file.
-    """
-
-    with open(filename, "w") as f:
+    with open(filename, "w") as file:
         json.dump(data, file)
 
 
-
 def load_and_deserialize(filename):
-    """
-    Load and deserialize JSON data from a file.
-    """
-
-    with open(filename, "r") as f:
+    with open(filename, "r") as file:
         return json.load(file)
