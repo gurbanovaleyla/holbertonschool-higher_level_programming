101f8fa7b8b63382f2a9af060918273412831258
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-05-21 18:53:25 +0200

    test file added

---
+++ python-serialization/test.py
@@ -0,0 +1,19 @@
+from task_00_basic_serialization import (
+    serialize_and_save_to_file,
+    load_and_deserialize
+)
+
+data = {
+    "name": "Leyla",
+    "age": 26,
+    "city": "Warsaw"
+}
+
+serialize_and_save_to_file(data, "data.json")
+
+print("Saved!")
+
+result = load_and_deserialize("data.json")
+
+print("Loaded result:")
+print(result)
