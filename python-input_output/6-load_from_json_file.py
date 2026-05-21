d0561472e7640c652400a5dd58c71d951b4c58be
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-05-21 12:57:45 +0200

    reading from json file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that creates a Python object from a JSON file.
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """Create an object from a JSON file."""
+    return json.load(f)
