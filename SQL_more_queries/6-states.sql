bd1d8ff4067b59b5ce3c70f119c7121367cb0a66
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-05-22 21:18:54 +0200

    creates database hbtn_0d_usa and table states

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,9 +1,9 @@
 -- creates database hbtn_0d_usa and table states
-i
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
