a9e77521d9f4f5ce3c0ca7e09ff05fd2ba6eed5e
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-05-22 21:45:48 +0200

    lists all shows without a genre linked (genre_id is NULL)

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,17 +1,18 @@
 -- 12-no_genre.sql
--- lists all shows without a genre linked (genre_id is NULL)
+-- lists all shows without a genre linked
 
 SELECT
     tv_shows.title,
     tv_show_genres.genre_id
 FROM
-    tv_shows
-LEFT JOIN
+    tv_shows,
     tv_show_genres
-ON
-    tv_shows.id = tv_show_genres.show_id
 WHERE
-    tv_show_genres.genre_id IS NULL
+    tv_shows.id = tv_show_genres.show_id
+    AND tv_shows.id NOT IN (
+        SELECT show_id
+        FROM tv_show_genres
+    )
 ORDER BY
     tv_shows.title ASC,
     tv_show_genres.genre_id ASC;
