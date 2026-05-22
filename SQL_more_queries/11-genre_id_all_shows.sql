fc4cc787810df0026cc679e051ea3075e57a346e
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-05-22 21:39:35 +0200

    lists all shows and their genre_id (including shows without genres)

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,15 @@
+-- 11-genre_id_all_shows.sql
+-- lists all shows and their genre_id (including shows without genres)
+
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows
+LEFT JOIN
+    tv_show_genres
+ON
+    tv_shows.id = tv_show_genres.show_id
+ORDER BY
+    tv_shows.title ASC,
+    tv_show_genres.genre_id ASC;
