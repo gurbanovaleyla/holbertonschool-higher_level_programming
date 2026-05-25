d2070773648d949eb941720835700e94d1011b17
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-05-25 21:49:52 +0200

    Add list item on click

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+document.querySelector('#add_item').addEventListener('click', function () {
+  const li = document.createElement('li');
+  li.textContent = 'Item';
+
+  document.querySelector('.my_list').appendChild(li);
+});
